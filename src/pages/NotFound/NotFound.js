9a334815d359494a78cf8349ff95c3443178181a
shadikmahamudsohan <shadikmahamudsohan@gmail.com>
2022-04-16 22:12:58 +0600

    all component file created

---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>404</h1>
+            <h4>Page not found</h4>
+            <button>Go Back Home</button>
+        </div>
+    );
+};
+
+export default NotFound;
